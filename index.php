e70c7a20778fc573c88092fe3caf910ceaf99da1
Larissa Julie <128844505+jlarissaf@users.noreply.github.com>
2026-01-29 23:10:48 -0300

    Commit index.php

--- index.php
+++ index.php
@@ -1,20 +1,3 @@
 <?php
-
-require_once "conexao.php";
-$consulta = "SELECT * FROM tarefas";
-$con = $mysqli
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tarefas</title>
-    <style>
-        
-    </style>
-</head>
-<body>
-    
-</body>
-</html>
+header("Location: inicial.php");
+exit;
