561e1407197513f38c9bbbf3e8de3a8aa05890f1
Larissa Julie <128844505+jlarissaf@users.noreply.github.com>
2026-01-29 23:09:55 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -10,3 +10,22 @@ $mysqli = new mysqli($host, $usuario, $senha, $banco);
 if ($mysqli->connect_error) {
     die("Erro na conexão com o banco de dados.");
 }
+
+
+///para hospedagem
+<?php
+
+$mysqli = new mysqli(
+    "sql300.infinityfree.com",
+    "if0_41027526",
+    "querocomer24",
+    "if0_41027526_root",
+    3306
+);
+
+if ($mysqli->connect_error) {
+    die("Erro de conexão: " . $mysqli->connect_error);
+}
+
+$mysqli->set_charset("utf8");
+
